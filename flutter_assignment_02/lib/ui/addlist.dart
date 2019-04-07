8bb216715b6de4801384800235c32ff20711a1da
jacktnp <thanaponpkw@gmail.com>
2019-04-07 19:10:57 +0700

    :balloon: version 1.0.0

---
+++ flutter_assignment_02/lib/ui/addlist.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../model/database.dart';
+
+class AddTodo extends StatefulWidget {
+  @override
+  _AddTodoState createState() => _AddTodoState();
+}
+
+class _AddTodoState extends State<AddTodo> {
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  TodoProvider _db;
+
+  _AddTodoState() {
+    _db = TodoProvider();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _db.open().then((result) {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController subjectController = TextEditingController();
+
+    TextFormField todoTextField = TextFormField(
+      controller: subjectController,
+      decoration: InputDecoration(
+        hintText: "Subject",
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter text';
+        }
+      },
+    );
+
+    RaisedButton submitButton = RaisedButton(
+      child: const Text('Save'),
+      onPressed: () {
+        if (_formKey.currentState.validate()) {
+          Todo todo = Todo(subject: subjectController.text);
+          _db.insert(todo).then((r) {
+            Navigator.pushReplacementNamed(context, '/');
+          });
+        }
+      },
+    );
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: Text('New Subject'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: EdgeInsets.all(12.0),
+          child: ListView(
+            children: <Widget>[
+              todoTextField,
+              submitButton,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_assignment_02/lib/ui/homescreen.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import '../model/database.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new TodoListState();
+  }
+}
+
+class TodoListState extends State {
+  int _index = 0;
+
+  List<Todo> _todoItems = List();
+  List<Todo> _doneItems = List();
+
+  TodoProvider _db;
+
+  TodoListState() {
+    _db = TodoProvider();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _db.open().then((result) {
+      getTodos();
+    });
+  }
+
+  void getTodos() {
+    _db.getTodos().then((r) {
+      setState(() {
+        _todoItems = r;
+      });
+    });
+  }
+
+  void getDone() {
+    _db.getDones().then((r) {
+      setState(() {
+        _doneItems = r;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> _children = [
+      _todoItems.length == 0
+          ? Text('No data found..')
+          : ListView(
+              children: _todoItems.map((todo) {
+                return CheckboxListTile(
+                  title: Text(todo.subject),
+                  value: todo.done,
+                  onChanged: (bool value) {
+                    setState(() {
+                      todo.done = value;
+                      _db.setTodoDone(todo);
+                      getTodos();
+                    });
+                  },
+                );
+              }).toList(),
+            ),
+      _doneItems.length == 0
+          ? Text('No data found..')
+          : ListView(
+              children: _doneItems.map((todo) {
+                return CheckboxListTile(
+                  title: Text(todo.subject),
+                  value: todo.done,
+                  onChanged: (bool value) {
+                    setState(() {
+                      todo.done = value;
+                      _db.setTodoDone(todo);
+                      getDone();
+                    });
+                  },
+                );
+              }).toList(),
+            )
+    ];
+
+    final List topBar = <Widget>[
+      IconButton(
+        icon: Icon(Icons.add),
+        onPressed: () {
+          Navigator.pushNamed(context, '/add');
+        },
+      ),
+      IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: () {
+          _db.deleteDone().then((_) {
+            getDone();
+          });
+        },
+      )
+    ];
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Todo"),
+        actions: <Widget>[topBar[_index]],
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(child: _children[_index]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.list), title: Text('Task')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.done_all), title: Text("Completed"))
+        ],
+        onTap: (int index) {
+          setState(() {
+            _index = index;
+            if (index == 0) {
+              getTodos();
+            } else {
+              getDone();
+            }
+          });
+        },
+      ),
+    );
+  }
+}
